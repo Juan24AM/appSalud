0eba87d0524e1916e5f2f66ad6a45890d18b588d
ruzvex <rs.daysuu@gmail.com>
2024-11-25 14:42:11 -0500

    test_env.php para verificar la carga de variables de entorno

--- public/test_env.php
+++ public/test_env.php
@@ -1,1 +1,8 @@
 <?php
+require_once '../config/env.php'; // Asegúrate de que esta ruta sea correcta
+loadEnv(__DIR__ . '/../.env'); // Ajusta la ruta al archivo .env
+
+echo 'DB_HOST: ' . $_ENV['DB_HOST'] . '<br>';
+echo 'DB_NAME: ' . $_ENV['DB_NAME'] . '<br>';
+echo 'DB_USERNAME: ' . $_ENV['DB_USERNAME'] . '<br>';
+echo 'DB_PASSWORD: ' . $_ENV['DB_PASSWORD'] . '<br>';
