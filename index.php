1a8cadda5f2fe42e7305de1991d82434bed7ed33
ruzvex <rs.daysuu@gmail.com>
2024-11-27 07:03:14 -0500

    Actualización en index.php: reorganización del flujo principal de la aplicación.

--- index.php
+++ index.php
@@ -1,31 +1,46 @@
 <?php
-session_start();
+spl_autoload_register(function ($class) {
+    $prefixes = [
+        'App\\Controllers\\' => '/app/controllers/',
+        'App\\Models\\' => '/app/models/',
+        'App\\Config\\' => '/config/'
+    ];
 
-require_once __DIR__ . '/app/controllers/RegisterController.php';
-require_once __DIR__ . '/app/controllers/LoginController.php';
-require_once __DIR__ . '/app/controllers/ProfileController.php';
-require_once __DIR__ . '/app/controllers/Error404Controller.php';
+    foreach ($prefixes as $prefix => $dir) {
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) === 0) {
+            $relative_class = substr($class, $len);
+            $file = __DIR__ . $dir . $relative_class . '.php';
+
+            if (file_exists($file)) {
+                require_once $file;
+                return;
+            }
+        }
+    }
+});
+
+use App\Controllers\Error404Controller;
 
 define('BASE_URL', '/appSalud');
 
 $request = $_SERVER['REQUEST_URI'];
 
-// Eliminar parámetros de consulta de la URL
 $request = strtok($request, '?');
 
 switch ($request) {
     case BASE_URL . '/register':
-        $controller = new RegisterController();
+        $controller = new \App\Controllers\RegisterController();
         $controller->register();
         break;
 
     case BASE_URL . '/login':
-        $controller = new LoginController();
+        $controller = new \App\Controllers\LoginController();
         $controller->login();
         break;
 
     case BASE_URL . '/profile':
-        $controller = new ProfileController();
+        $controller = new \App\Controllers\ProfileController();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $controller->updateProfile();
         } else {
@@ -34,17 +49,17 @@ switch ($request) {
         break;
 
     case BASE_URL . '/logout':
-        $controller = new LoginController();
+        $controller = new \App\Controllers\LoginController();
         $controller->logout();
         break;
 
     case BASE_URL . '/':
     case BASE_URL:
-        // Redirigir a la página de inicio de sesión
         header("Location: " . BASE_URL . "/login");
         exit();
 
     default:
+
         $errorController = new Error404Controller();
         $errorController->show404();
         break;
