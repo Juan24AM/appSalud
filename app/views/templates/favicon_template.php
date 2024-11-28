740d11d5d33d3604f5c7f090ed1e36ecfa693a51
ruzvex <rs.daysuu@gmail.com>
2024-11-28 08:03:58 -0500

    Modificado el archivo favicon_template.php para incluir un icono más adecuado

--- app/views/templates/favicon_template.php
+++ app/views/templates/favicon_template.php
@@ -1,1 +1,4 @@
-<?php
+<link rel="icon" href="<?= BASE_URL ?>/public/images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?= BASE_URL ?>/public/images/favicon.ico" type="image/x-icon">
+<link rel="icon" sizes="192x192" href="<?= BASE_URL ?>/public/images/favicon-192x192.png">
+<link rel="icon" sizes="512x512" href="<?= BASE_URL ?>/public/images/favicon-512x512.png">
